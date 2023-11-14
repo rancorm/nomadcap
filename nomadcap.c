9ae1a02803c93a3c1aa9c7282d96e4a3cb2cf6ef
Jonathan Cormier <jonathan@cormier.co>
2023-11-14 19:09:46 -0400

    Add find devs code, functions, switches, PCAP
    Include PCAP header pcap.h.
    Add function nomadcap_exit to handle exit clean up.
    Add function nomadcap_interesting to parse packets.
    Add structs nomadcap_pack_t and nomadcap_entry_t for application state and hash entries.
    Add PCAP related defines and errbuf variable.
    Add pack and hash_table variables respectfully.
    Update command argument parsing to handle n and i switches.
    Add code to locate interface when not provided.

--- nomadcap.c
+++ nomadcap.c
@@ -8,32 +8,90 @@
 #include <unistd.h>
 #include <string.h>
 
+#include <pcap.h>
+
+/* getopt friends */
 extern char *optarg;
 extern int optind, opterr, optopt;
 
 #include "nomadcap.h"
 
+void
+nomadcap_exit(nomadcap_pack_t *pack, int code) {
+    /* Clean up */
+    if (pack->device) {
+        free(pack->device);
+    }
+
+    exit(code);
+}
+
+int
+nomadcap_interesting(nomadcap_pack_t *pack,
+    unsigned char *packet,
+    nomadcap_entry_t **hash_table) {
+}
+
 int
 main(int argc, char *argv[]) {
+    nomadcap_pack_t pack;
+    nomadcap_entry_t *hash_table[NOMADCAP_TABLE_SIZE];
+    pcap_if_t *alldevs;
+    char errbuf[PCAP_ERRBUF_SIZE];
     int c = -1;
-    char *device;
 
-    /* Defaults */
-    device = NOMADCAP_INTF;
+    /* Init */
+    pack.device = NULL;
+    pack.filter = NOMADCAP_FILTER;
+    pack.threshold = NOMADCAP_THRESHOLD;
+    pack.flags = NOMADCAP_FLAGS_NONE;
 
     /* Parse command line argumemnts */
     while ((c = getopt(argc, argv, NOMADCAP_OPTS)) != -1) {
         switch (c) {
+            case 'n':
+                pack.threshold = atoi(optarg);
+                break;
             case 'i':
-                device = strdup(optarg);
+                pack.device = strdup(optarg);
+                break;
+            case 'v':
+                pack.flags |= NOMADCAP_FLAGS_VERB;
                 break;
             default: /* '?' */
-                fprintf(stderr, "Usage: %s [-i intf]\n", argv[0]);
+                fprintf(stderr, "Usage: %s [-n #] [-i intf] [-hv]\n", argv[0]);
                 exit(EXIT_FAILURE);
         }
     }
 
-    printf("Listening on: %s\n", device);
+    /* Leave it to libpcap to find an interface */
+    if (pack.device == NULL) {
+        /* Find all available network interfaces */
+        if (pcap_findalldevs(&alldevs, errbuf) == -1) {
+            fprintf(stderr, "pcap_findalldevs: %s\n", errbuf);
+            nomadcap_exit(&pack, EXIT_FAILURE);
+        }
 
-    return 0;
+        /* No interfaces, print an error message and exit */
+        if (alldevs == NULL) {
+            fprintf(stderr, "No interfaces found\n");
+            nomadcap_exit(&pack, EXIT_FAILURE);
+        }
+
+        /* Copy device name of first found device */
+        pack.device = strdup(alldevs[0].name);
+
+        /* Free the list of interfaces */
+        pcap_freealldevs(alldevs);
+    }
+
+    printf("Frame threshold: %d\n", pack.threshold);
+    printf("Listening on: %s\n", pack.device);
+
+    /* Verbose details.. */
+    if (pack.flags & NOMADCAP_FLAGS_VERB) {
+        printf("Filter: %s\n", NOMADCAP_FILTER);
+    }
+
+    nomadcap_exit(&pack, EXIT_SUCCESS);
 }
--- nomadcap.h
+++ nomadcap.h
@@ -1,13 +1,40 @@
 #ifndef __NOMADCAP_H
 #define __NOMADCAP_H
 
-/* IEEE OUI data */
-#define NOMADCAP_OUI_PATH "/usr/share/ieee-data/oui.txt"
-#define NOMADCAP_OUI_FILE NOMADCAP_OUI_PATH ## "oui.txt"
+/* IEEE OUI path & files */
+#define NOMADCAP_OUI_PATH "/usr/share/ieee-data/"
+#define NOMADCAP_OUI_FILE NOMADCAP_OUI_PATH ## "oui.csv"
 
-#define NOMADCAP_INTF "lo0"
-#define NOMADCAP_OPTS "i:h"
+/* PCAP stuff */
+/* Ethernet ARP broadcast requests */
+#define NOMADCAP_FILTER "(arp.opcode == 1) && (eth.dst == ff:ff:ff:ff:ff:ff)"
+#define NOMADCAP_SNAPLEN 34
+#define NOMADCAP_TIMEOUT 500
+#define NOMADCAP_PROMISC 1
+
+#define NOMADCAP_TABLE_SIZE 1009
+#define NOMADCAP_THRESHOLD 1
+#define NOMADCAP_OPTS "n:i:hv"
 
 #define NOMADCAP_VERSION "0.1"
+
+#define NOMADCAP_FLAGS_NONE 0
+#define NOMADCAP_FLAGS_VERB 0x1
+
+/* Package */
+typedef struct nomadcap_pack {
+    char *device;
+    char *filter;
+    int threshold;
+    unsigned char flags;
+} nomadcap_pack_t;
+
+/* Hash entry */
+typedef struct nomadcap_entry {
+    /* MAC address */
+    unsigned char mac[6];
+
+    struct nomadcap_entry *next;
+} nomadcap_entry_t;
 
 #endif /* __NOMADCAP_H */
